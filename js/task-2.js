7bf854d34bad640d8c54e47598c1813d0901c978
Анжеліка <162304677+milajalika1@users.noreply.github.com>
2024-06-17 14:03:13 +0300

    update task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 function formatMessage(message, maxLength) {
             if (message.length > maxLength) {
-        return message.slice(message, maxLength) + "...";
+        return message.slice(0, maxLength) + "...";
     }
     else {
         return message;
